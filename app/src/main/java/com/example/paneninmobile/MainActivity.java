7671c6582d8c1ecb25354b3539a14d5c81d7bd06
viviashilah14 <viviashilah14@gmail.com>
2023-05-12 18:31:09 +0700

    Pengerjaan Login & Register
    Untuk form login dan register sudah selesai dibuat, semua button telah berfungsi kecuali button lupa password, ada kendala dalam pengaturan layout frontend dimana tidak dapat menyesuaikan sesuai dengan emulator setiap emulator diganti harus kembali menyesuaikan layout desain agar sesuai dengan emulator yang baru.

--- app/src/main/java/com/example/paneninmobile/MainActivity.java
+++ app/src/main/java/com/example/paneninmobile/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.paneninmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_login, btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+        btn_register = findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/paneninmobile/RegisterActivity.java
@@ -0,0 +1,37 @@
+package com.example.paneninmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+    ImageView btn_back;
+    TextView btn_tologin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        btn_back = findViewById(R.id.back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+            }
+        });
+
+        btn_tologin = findViewById(R.id.tologin);
+        btn_tologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+}
